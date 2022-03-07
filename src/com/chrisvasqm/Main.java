1131681f41e1bc35d02c2161a2bcbe7068d72194
chrisvasqm <christianvasquezmelo@gmail.com>
2022-03-07 18:20:14 -0400

    Clean up code

--- src/com/chrisvasqm/Main.java
+++ src/com/chrisvasqm/Main.java
@@ -1,15 +1,7 @@
 package com.chrisvasqm;
 
-import com.chrisvasqm.arrays.Array;
-
 public class Main {
-
     public static void main(String[] args) {
-        Array numbers = new Array(3);
-        numbers.insert(1);
-        numbers.insert(2);
-        numbers.insert(3);
-
-        System.out.println(numbers.max());
+        // Code goes here :)
     }
 }
